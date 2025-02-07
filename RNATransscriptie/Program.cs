0c023f83912106bf973c98c90420273f676e4231
SvenJ-Atheneum <sven.janssens@atheneumsinttruiden.be>
2025-02-07 11:02:19 +0100

    Add project files.

---
+++ RNATransscriptie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RNATransscriptie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TafelsVanSupervermenigvuldiging/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TafelsVanSupervermenigvuldiging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
